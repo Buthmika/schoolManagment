df0044b22481455866ceb78b3563bb8adbb47918
Buthmika <buthmikamenushan@gmail.com>
2025-04-20 10:12:41 +0530

    Update Login.jsx

--- schoolstat/src/components/loginPage/Login.jsx
+++ schoolstat/src/components/loginPage/Login.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import './Login.css'
+import background from '../../assets/image.png'
+
 function Login() {
   return (
-    <div>
+    <div className="login-wrapper" style={{ backgroundImage: `url(${background})` }}>
       <h1>Login to SchoolStat</h1>
       <div className="info">
-            <div className="userName">
-                  <label htmlFor="username">Username</label>
-                  <input type="text" id="username" name='userName' placeholder="Enter your username" required/>
-            </div>
-            <div className="password">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" id="password" name='password' placeholder="Enter your password" required/>
-            </div>
+        <div className="userName">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name='userName' placeholder="Enter your username" required />
+        </div>
+        <div className="password">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name='password' placeholder="Enter your password" required />
+        </div>
       </div>
-      
     </div>
   )
 }
