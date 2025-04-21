e3b90c58e8f72d11f37effc5cc8be3d614b97a13
Buthmika <buthmikamenushan@gmail.com>
2025-04-21 09:00:21 +0530

    Update Login.jsx

--- schoolstat/src/components/loginPage/Login.jsx
+++ schoolstat/src/components/loginPage/Login.jsx
@@ -9,12 +9,15 @@ function Login() {
       <div className="info">
       <h1>Login to SchoolStat</h1>
         <div className="userName">
-          <label htmlFor="username">Username</label>
+          <label htmlFor="username"><b>Username</b></label>
           <input type="text" id="username" name='userName' placeholder="Enter your username" required />
         </div>
         <div className="password">
-          <label htmlFor="password">Password</label>
+          <label htmlFor="password"><b>Password</b></label>
           <input type="password" id="password" name='password' placeholder="Enter your password" required />
+        </div>
+        <div className="submit">
+          <button type="submit">Login</button>
         </div>
       </div>
     </div>
