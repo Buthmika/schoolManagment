11d5ec84c7b45885ce7d4edc7d4936da9a5de358
Buthmika <buthmikamenushan@gmail.com>
2025-04-22 17:51:38 +0530

    Update App.jsx

--- schoolstat/src/App.jsx
+++ schoolstat/src/App.jsx
@@ -1,16 +1,24 @@
 import HomePage from "./components/homePage/HomePage"
 import Login from "./components/loginPage/Login"
 import SignUp from "./components/signupPage/SignUp"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 
 function App() {
  
 
   return (
+    
     <>
-      <HomePage></HomePage>
-      {/* <Login></Login> */}
-      {/* <SignUp></SignUp> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+      
     </>
   )
 }
