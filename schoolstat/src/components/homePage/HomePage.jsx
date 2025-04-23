ed7f129ac7fa62742d1df40a8424ffdbfc63d015
Buthmika <buthmikamenushan@gmail.com>
2025-04-23 15:23:22 +0530

    Update HomePage.jsx

--- schoolstat/src/components/homePage/HomePage.jsx
+++ schoolstat/src/components/homePage/HomePage.jsx
@@ -8,20 +8,24 @@ function HomePage() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <Nav />
-    <div  className="login-wrapper" style={{ backgroundImage: `url(${HomePageImage})` }}>
-      
+    <div className="login-wrapper" style={{ backgroundImage: `url(${HomePageImage})` }}>
+      <div className="navi">
+        <Nav />
+      </div>
+
       <div className="mainContent">
-        <div className="mainImage">
-          
+        <div className="home-container">
+          <h1>Welcome to <span className="highlight">SchoolStat</span></h1>
+          <p>Get your Leaving & Character Certificates online</p>
+          <h2>Simple & Fast Certificate Requests</h2>
+          <p>Apply online with ease</p>
+        </div>
+
+        <div className="button">
+          <button onClick={() => navigate('/SignUp')}>Sign Up</button>
+          <button onClick={() => navigate('/Login')}>Login</button>
         </div>
       </div>
-      <div className="button">
-        <button onClick={()=>navigate('/SignUp')}>Sign Up</button>
-        <button onClick={()=>navigate('/Login')}>Login</button>
-      </div>
-    </div>
     </div>
   );
 }
