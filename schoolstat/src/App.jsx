ceed7e410f21f5a6daa91e0b70321e829e14ef80
Buthmika <buthmikamenushan@gmail.com>
2025-05-07 10:42:36 +0530

    Update App.jsx

--- schoolstat/src/App.jsx
+++ schoolstat/src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from "./components/homePage/HomePage"
 import LandingPage from "./components/LandingPage/LandingPage";
 import Login from "./components/loginPage/Login"
 import SignUp from "./components/signupPage/SignUp"
+import MarkPage from "./components/markPage/MarkPage"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -12,16 +13,17 @@ function App() {
   return (
     
     <>
-    {/* <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/markPage" element={<MarkPage />} />
+        <Route path="/landingPage" element={<LandingPage />} />
       </Routes>
-    </Router> */}
-    <LandingPage></LandingPage>
-      
-    </>
+    </Router>
+    {/* <LandingPage></LandingPage> */}
+?    </>
   )
 }
 
