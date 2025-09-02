0b06ca2231423bcee0bb821d5d7ba87c50ddf208
Buthmika <buthmikamenushan@gmail.com>
2025-09-02 19:46:59 +0530

    Fix Firebase storageBucket config value
    Corrected the storageBucket value in firebaseConfig.js from 'schoolstat-d77e6.firebasestorage.app' to 'schoolstat-d77e6.appspot.com' to ensure proper Firebase storage integration.

--- schoolstat/src/firebaseConfig.js
+++ schoolstat/src/firebaseConfig.js
@@ -4,10 +4,10 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-   apiKey: "AIzaSyAyYUuq8F_1oG-b09Mb5AaY4Xfp32630cQ",
+  apiKey: "AIzaSyAyYUuq8F_1oG-b09Mb5AaY4Xfp32630cQ",
   authDomain: "schoolstat-d77e6.firebaseapp.com",
   projectId: "schoolstat-d77e6",
-  storageBucket: "schoolstat-d77e6.firebasestorage.app",
+  storageBucket: "schoolstat-d77e6.appspot.com", // <-- FIXED
   messagingSenderId: "752810793800",
   appId: "1:752810793800:web:34b780d9b7cd3d2cd9317d",
   measurementId: "G-7MD5H7J1D1"
